d79c38f3e1bec1135c070ae6c9ef61b377525e9a
ha382 <ha382@njit.edu>
2020-06-20 09:04:06 -0400

    register

--- register.php
+++ register.php
@@ -1,4 +1,5 @@
 <?php
+/*
 require('config.php');
 $conn_string = "mysql:host=$host;dbname=$database;charset=utf8mb4";
 $db = new PDO($conn_string, $username, $password);
@@ -12,7 +13,7 @@ $result = $stmt->fetch();
   $insert_query = "INSERT INTO `ha382`.`testtable` (`id`, `email`, `password`) VALUES (NULL, '$enteredEmail', '$enteredPassword');";
   $stmt = $db->prepare($insert_query);
   $r = $stmt->execute();
-  
+  */
   //SOMEHOW PERSIST LOGIN VALUES INTO DASHBOARD
   header("Location: registerSuccess.html");
   
