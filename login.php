344b965046a1936b75b3ffa8beb02247d66241fb
ha382 <ha382@njit.edu>
2020-06-20 12:00:32 -0400

    login

---
+++ login.php
@@ -0,0 +1,33 @@
+<?php
+session_start();
+
+$host="sql2.njit.edu";
+$database="ha382";
+$username="ha382";
+$password="Ywcc!1xcvb";
+
+$conn_string = "mysql:host=$host;dbname=$database;charset=utf8mb4";
+$db = new PDO($conn_string, $username, $password);
+
+
+$stmt = $db->query("SELECT * from testtable");
+$result = $stmt->fetch();
+
+
+  $enteredEmail = $_POST['Lemail'];
+  $enteredPassword = $_POST['Lpassword'];
+
+  $stmt = $db->query("SELECT * FROM testtable WHERE email = '$enteredEmail'");
+  $result = $stmt->fetch();
+
+  if($enteredPassword == $result['password'])
+
+
+  /*
+  $insert_query = "INSERT INTO `ha382`.`testtable` (`email`, `password`) VALUES ('$enteredEmail', '$enteredPassword');";
+  $stmt = $db->prepare($insert_query);
+  $r = $stmt->execute();
+  */
+
+  header("Location: loginSuccess.html");
+?>
