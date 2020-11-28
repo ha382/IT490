0cd6228d3929fc917cf180013d739ca256573b5f
ha382 <ha382@njit.edu>
2020-11-28 16:19:59 -0500

    change for is117

--- create_table.sql
+++
@@ -1,6 +0,0 @@
-create table if not exists `testtable`(
-		`id` int auto_increment not null,
-		`email` varchar(30) not null unique,
-		`password` varchar(30) not null,
-		PRIMARY KEY (`id`)
-		) CHARACTER SET utf8 COLLATE utf8_general_ci;
